67bbdf5e767ead64978991026bc24060f4ad1a91
Suryansh Shrivastava <suryansh555@yahoo.in>
2023-01-11 17:26:25 +0530

    Added More Problems

---
+++ Basic Programming/Pile of Coins.cpp
@@ -0,0 +1,3 @@
+#include <iostream> 
+using namespace std; 
+int main() {     int t;     cin>>t;     while(t--)     {         int p,q;         cin>>p>>q;         if(p%2==0 || q%2==0) cout<<"Ashish"<<endl;         else cout<<"Jeel"<<endl;              }     return 0; }
