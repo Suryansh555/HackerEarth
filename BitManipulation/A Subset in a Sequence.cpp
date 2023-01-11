b6f437556daf1f07c4bb61acea9a2cc60699ef5b
Suryansh Shrivastava <suryansh555@yahoo.in>
2023-01-11 17:20:37 +0530

    Added More Problems

--- BitManipulation/A Subset in a Sequence.cpp
+++ BitManipulation/A Subset in a Sequence.cpp
@@ -1,87 +1,56 @@
-#include<bits/stdc++.h>
-
-#define ll long long
-
-#define pb push_back
-
-#define mod 1000000007
-
-#define vi vector<int>
-
-#define FOR(i, n) for(int i=0; i<n; i++)
-
-#define FORR(i, n) for(int i=1; i<=n; i++)
-
-#define fi first
-
-#define se second
-
+#include <bits/stdc++.h>
 using namespace std;
-
-int main()
-
+long pon(long,long);
+ 
+ 
+//power function is for printing all number.
+void power(long long p)
 {
-
-    ios_base::sync_with_stdio(false);
-
-    cin.tie(NULL);
-
-    cout.tie(NULL);
-
-    
-
-    ll tc,n,i,j,cnt;
-
-    ll arr[40];
-
-    arr[0]=1;
-
-    for(i=1;i<40;i++){
-
-        arr[i] = arr[i-1]*3;
-
-    }
-
-    
-
-    cin>>tc;
-
-    while(tc--){
-
-        cin>>n;
-
-        vector<ll> bin;
-
-        ll cnt=0;
-
-        while(n>0){
-
-            bin.pb(n%2);
-
-            if(n%2==1) cnt++;
-
-            n /= 2;
-
-        }
-
-        
-
-        cout<<cnt<<'\n';
-
-        
-
-        for(i=0;i<bin.size();i++){
-
-            if(bin[i] == 1) cout<<arr[i]<<" ";
-
-        }
-
-        cout<<'\n';
-
-    }
-
-        
-
-    return 0;
-
+   long i=0;
+   while(p)
+       {
+           if(p%2==1)
+           {
+               cout<<pon(p,i)<<' ';
+           }
+           p=p/2;
+           i++;
+       }
+       cout<<'\n';
+}
+ 
+ 
+ 
+//pon function is for power of 3.
+long pon(long p,long i)
+{
+   long j,k=1;
+   for(j=1;j<=i;j++)
+       k*=3;
+   return k;
+} 
+ 
+ 
+int main ()
+{
+   int T;
+   long n,i=0,p;
+   cin>>T;
+   while(T--)
+   {
+       cin>>n;
+       p=n;
+       while(n)
+       {
+           if(n%2==1)
+           {
+               i++;
+           }
+           n=n/2;
+       }
+       cout<<i<<'\n';
+       power(p);
+       i=0;
+   }
+   return 0;
 }
